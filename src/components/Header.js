5c0e40e3474ed00bd63422c0b1af277bdc93de65
young <peacestrokes@gmail.com>
2022-01-25 01:27:36 +0900

    Add header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "../header.css"
 
 function Header() {
     return (
         <header>
-            <nav>Header Part</nav>
+            <nav className="navbar">
+                <div className="navbar__title">
+                    <a tabIndex={0}>MOVIES</a>
+                </div>
+                <ul className="navbar__menu">
+                    <li className="navbar__menu-item">
+                        <a href="#">_hello</a>
+                    </li>
+                    <li className="navbar__menu-item">
+                        <a href="#">_career</a>
+                    </li>
+                    <li className="navbar__menu-item">
+                        <a href="#">_project</a>
+                    </li>
+                    <li className="navbar__menu-item">
+                        <a href="#">_dream</a>
+                    </li>
+                </ul>
+                <ul className="navbar__misc">
+                    <li className="navbar__icon">dd</li>
+                    <li className="navbar__icon">ss</li>
+                </ul>
+            </nav>
+
         </header>
     )
 }
