add612f34f1c0b8d2f05677499ac6d1358cd51ec
young <peacestrokes@gmail.com>
2022-03-23 01:39:15 +0900

    Update Search.js form and style

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,17 +1,16 @@
 import React, {useCallback, useEffect, useState} from "react";
 import Movies from "../components/Movies"
-import "../_movies.scss"
 import {useLocation} from "react-router-dom";
 import { useForm } from "react-hook-form";
+import "../_movies.scss"
+import "../_search.scss"
 
 function Search() {
     const location = useLocation();
     const [loading, setLoading] = useState(false);
     const [movies, setMovies] = useState([])
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
     // https://yts.mx/api/v2/list_movies.json?quality=3D
-
     useEffect(() => {
         console.log(location)
         if (location.state) {
@@ -21,6 +20,27 @@ function Search() {
         }
     }, [location])
 
+    // useForm
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = async (data) => {
+        console.log(data)
+        console.log((`
+            https://yts.mx/api/v2/list_movies.json?
+            query_term=${data.queryTerm}&
+            quality=${data.quality}&
+            with_rt_ratings=true
+            `))
+        let result = await (
+            await fetch(`
+            https://yts.mx/api/v2/list_movies.json?
+            query_term=${data.queryTerm}&
+            quality=${data.quality}&
+            with_rt_ratings=true
+            `)
+        ).json()
+        setMovies(result.data.movies)
+    }
+
     return (
         <>
             {loading
@@ -29,11 +49,35 @@ function Search() {
                 <>
                     <section className="container">
                         <div className="cover cover-search flex__center">
-                            <form>
-                                <input type={"text"}/>
+                            <form onSubmit={handleSubmit(onSubmit)}
+                                  className={"search"}>
+                                <label className={"search__label"}>검색어</label>
+                                <input className={"search__input"}
+                                       {...register("queryTerm")} />
+                                <label className={"search__label"}>최소 평점</label>
+                                <select className={"search__input"}
+                                        {...register("minimumRating")}>
+                                    <option value="">해당 없음</option>
+                                    {[...Array(10)].map((_, i)=>i)
+                                        .map((int, key) => (
+                                        <option value={`${int}`} key={key}>{int}</option>
+                                    ))}
+                                </select>
+                                <label className={"search__label"}>화질</label>
+                                <select className={"search__input"}
+                                    {...register("quality")}>
+                                    <option value="">해당 없음</option>
+                                    <option value="720p">720p</option>
+                                    <option value="1080p">1080p</option>
+                                    <option value="2160p">2160p</option>
+                                    <option value="3D">3D</option>
+                                </select>
+                                <input type="submit" className={"search__submit"} value={"검색"} />
                             </form>
                         </div>
-                        <Movies movies={movies}/>
+                        {movies.length
+                            ? <Movies movies={movies}/>
+                            : <h2 className={"flex__center"}>결과가 없습니다.</h2>}
                     </section>
                 </>
             }
