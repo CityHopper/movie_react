346eb4a9e6d43ed33650d5acb63ba2b7cde853d2
young <peacestrokes@gmail.com>
2022-04-22 13:45:21 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom';
 import "../_footer.scss";
 
 function Footer() {
@@ -7,8 +6,14 @@ function Footer() {
     return (
         <footer className={"flex__center flex__column"}>
             <ul className={"footer__links"}>
-                <li><Link to={`https://github.com/CityHopper/react_movie`}>GitHub</Link></li>
-                <li><Link to={`https://is-this-it.tistory.com/`}>Blog</Link></li>
+                <li>
+                    <button onClick={() => window.open(`https://github.com/CityHopper/react_movie`)}>
+                        GitHub</button>
+                </li>
+                <li>
+                    <button onClick={() => window.open("https://is-this-it.tistory.com")}>
+                        Blog</button>
+                </li>
             </ul>
             <p>Copyright &copy; {thisYear}. CityHopper All Rights Reserved</p>
 
